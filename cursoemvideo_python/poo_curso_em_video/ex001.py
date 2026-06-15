e4c114c8afb05785295bcdb5a34446aef3f5cd52
Luissms <ldossant@gmail.com>
2026-06-15 20:37:09 -0300

    Create ex001.py

---
+++ cursoemvideo_python/poo_curso_em_video/ex001.py
@@ -0,0 +1,35 @@
+"""
+Classe é um molde, um projeto de algo que queremos construir.
+Ela é um modelo para criar objetos. Atributos são as características do objeto,
+e métodos são as ações que o objeto pode realizar. Instância é um objeto criado a partir 
+de uma classe, ou seja, é uma ocorrência específica da classe.
+
+"""
+# Declaração de Classe
+
+class Gafanhoto:
+    def __init__(self): # Método Construtor
+        # Atributos de Instância
+        self.nome = ""
+        self.idade = 0
+
+    # Métodos de Instância
+    def aniversario(self):
+        self.idade += 1
+
+    def mensagem(self):
+        return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade."
+    
+
+# Declaração de Objetos
+g1 = Gafanhoto() # Instância da Classe Gafanhoto
+g1.nome = "Maria" 
+g1.idade = 17
+g1.aniversario() # Chama o método para incrementar a idade
+print(g1.mensagem())
+
+g2 = Gafanhoto() # Outra Instância da Classe Gafanhoto
+g2.nome = "Mauro"
+g2.idade = 53
+g2.aniversario() # Chama o método para incrementar a idade
+print(g2.mensagem())
