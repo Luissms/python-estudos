f996a254d70384e9cd51a0add5f4f26d5dd18ba7
Luissms <ldossant@gmail.com>
2026-06-19 06:55:55 -0300

    Desafio 16
    Correção

--- cursoemvideo_python/poo_curso_em_video/desafios/desafio016.py
+++ cursoemvideo_python/poo_curso_em_video/desafios/desafio016.py
@@ -4,17 +4,27 @@ setor e cargo. Crie também um método que permita ao funcionário
 se apresentar.
 
 """
+from rich import print
+from rich import inspect
+
 class Funcionario:
+    # Atributos de classe
+    empresa = "Curso em Vídeo"
+
     def __init__(self, nome, setor, cargo):
+        # Atributos de instância
         self.nome = nome
         self.setor = setor
         self.cargo = cargo
 
     def apresentar(self):
-        return f"Olá, sou {self.nome} e sou {self.cargo} do setor de {self.setor} da empresa Curso em Vídeo."
+        return f":handshake: Olá, sou [blue]{self.nome}[/blue] e sou {self.cargo} do setor de {self.setor} da empresa {self.__class__.empresa}."
     
 c1 = Funcionario("Maria", "Administração", "Diretora")
 print(c1.apresentar())
+inspect(c1)
 
 c2 = Funcionario("Pedro", "TI", "Programador")
 print(c2.apresentar())
+inspect(c2)
+
