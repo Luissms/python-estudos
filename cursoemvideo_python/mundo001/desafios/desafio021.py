19bd41988524d9bdf5e0f57e073cc033f4a35185
Luissms <ldossant@gmail.com>
2026-06-27 15:29:54 -0300

    Update desafio021.py

--- cursoemvideo_python/mundo001/desafios/desafio021.py
+++ cursoemvideo_python/mundo001/desafios/desafio021.py
@@ -1,3 +1,9 @@
 """
 Faça um programa que abra e reproduza o áudio de um arquivo MP3.
+Colar o arquivo na mesma pasta que está este exercício.
 """
+# import pygame
+# pygame.init()
+# pygame.mixer.music.load('ex021.mp3')
+# pygame.mixer.music.play()
+# pygame.event.wait()
