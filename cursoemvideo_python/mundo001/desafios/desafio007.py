e506d19dfc1bc38da4c3fc4dfd24311f500d81bd
Luissms <ldossant@gmail.com>
2026-06-27 02:35:37 -0300

    Update desafio007.py

--- cursoemvideo_python/mundo001/desafios/desafio007.py
+++ cursoemvideo_python/mundo001/desafios/desafio007.py
@@ -5,4 +5,4 @@ Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a su
 n1 = float(input('Digite a primeira nota: '))
 n2 = float(input('Digite a segunda nota: '))
 m = (n1 + n2)/2
-print('A média entre {} e {} é a {:.2f}.'.format(n1, n2, m))
+print('A média entre {} e {} é igual a {:.2f}.'.format(n1, n2, m))
