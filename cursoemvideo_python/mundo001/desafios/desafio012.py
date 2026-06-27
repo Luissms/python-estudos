7573cb2434d106376fd4f918bbb52fad25f16106
Luissms <ldossant@gmail.com>
2026-06-27 03:13:17 -0300

    Update desafio012.py

--- cursoemvideo_python/mundo001/desafios/desafio012.py
+++ cursoemvideo_python/mundo001/desafios/desafio012.py
@@ -2,6 +2,5 @@
 Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
 """
 preco = float(input('Digite o preço do produto: R$ '))
-desconto = preco * 0.05
-novo_preco = preco - desconto
-print('O preço original do produto é R$ {:.2f}. Com o desconto de 5%, o novo preço é R$ {:.2f}.'.format(preco, novo_preco))
+novo = preco - (preco * 5 / 100)
+print('O preço original do produto é R$ {:.2f}. Com o desconto de 5%, o novo preço é R$ {:.2f}.'.format(preco, novo))
