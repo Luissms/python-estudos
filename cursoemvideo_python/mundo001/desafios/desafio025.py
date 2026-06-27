a04e1c4c3582dd3165e1851e8d20a88035244606
Luissms <ldossant@gmail.com>
2026-06-27 18:32:09 -0300

    Update desafio025.py

--- cursoemvideo_python/mundo001/desafios/desafio025.py
+++ cursoemvideo_python/mundo001/desafios/desafio025.py
@@ -1,3 +1,5 @@
 """
 Crie um programa que leia um nome de uma pessoa e diga se ela tem "SILVA" no nome.
 """
+nome = str(input('Qual é seu nome completo? ')).strip()
+print('Seu nome tem Silva? {}'.format('silva' in nome.lower())) # ou 'SILVA' in nome.upper()
