a3cf913cc4c8253f511914e1c0c4a2364252b7d7
Luissms <ldossant@gmail.com>
2026-06-27 02:44:19 -0300

    Update desafio008.py

--- cursoemvideo_python/mundo001/desafios/desafio008.py
+++ cursoemvideo_python/mundo001/desafios/desafio008.py
@@ -1,7 +1,7 @@
 """
 Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
 """
-metros = float(input('Digite um valor em metros: '))
-centimetros = metros * 100
-milimetros = metros * 1000
-print('O valor de {} metros corresponde a {} centímetros e {} milímetros.'.format(metros, centimetros, milimetros))
+m = float(input('Digite um valor em metros: '))
+cm = m * 100
+mm = m * 1000
+print('O valor de {}m corresponde a {}cm e {}mm.'.format(m, cm, mm))
