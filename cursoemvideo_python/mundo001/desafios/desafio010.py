ae66e305e00fbb88e2b50154f515ff0afd7a2ccf
Luissms <ldossant@gmail.com>
2026-06-27 02:57:26 -0300

    Update desafio010.py

--- cursoemvideo_python/mundo001/desafios/desafio010.py
+++ cursoemvideo_python/mundo001/desafios/desafio010.py
@@ -2,6 +2,6 @@
 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
 Considere: US$ 1,00 = R$ 5,25
 """
-reais = float(input('Digite o valor em reais que você tem na carteira: R$ '))
-dolares = reais / 5.25
-print('Com R$ {:.2f} você pode comprar US$ {:.2f}.'.format(reais, dolares))
+real = float(input('Quanto dinheiro você tem na carteira: R$ '))
+dolar = real / 3.27
+print('Com R$ {:.2f} você pode comprar US$ {:.2f}.'.format(real, dolar))
