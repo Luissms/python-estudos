8b0833ca7071df6d9de3d39a4e32e6239ab1f396
Luissms <ldossant@gmail.com>
2026-06-27 02:28:43 -0300

    Update desafio006.py

--- cursoemvideo_python/mundo001/desafios/desafio006.py
+++ cursoemvideo_python/mundo001/desafios/desafio006.py
@@ -1,3 +1,10 @@
 """
-Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
+Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada. raiz também pode ser calculada como pow(n, (1/2))
 """
+
+n = float(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1/2)
+print('O dobro de {} vale {}.'.format(n, d))
+print('O triplo de {} vale {}. A raiz quadrada de {} é igual a {:.2f}.'.format(n, t, n, r))
