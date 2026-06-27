eb2d14ffddd56a79464e71e1d63b785d7e9bb278
Luissms <ldossant@gmail.com>
2026-06-27 13:42:00 -0300

    Update desafio017.py

--- cursoemvideo_python/mundo001/desafios/desafio017.py
+++ cursoemvideo_python/mundo001/desafios/desafio017.py
@@ -1,3 +1,15 @@
 """
 Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hiponenusa.
 """
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa vai medir {}.'.format(hi))
+
+# Ou fazendo com a biblioteca
+
+import math # ou from math import hypot
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = math.hypot(co, ca) # hi = hypot(co, ca)
+print('A hipotenusa vai medir {}.'.format(hi))
