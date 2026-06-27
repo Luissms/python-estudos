a082443b7a46751ab4a56ad931ad58a0d85f968c
Luissms <ldossant@gmail.com>
2026-06-27 18:04:46 -0300

    Update desafio022.py

--- cursoemvideo_python/mundo001/desafios/desafio022.py
+++ cursoemvideo_python/mundo001/desafios/desafio022.py
@@ -5,3 +5,14 @@ O nome com todas as letras minúsculas.
 Quantas letras ao todo (sem considerar espaços).
 Quantas letras tem o primeiro nome.
 """
+nome = input('Digite o seu nome completo: ').strip()
+print('Analisando o seu nome...')
+print('Seu nome em maiúsculas é {}'.format(nome.upper()))
+print('Seu nome em minúsculas é {}'.format(nome.lower()))
+print('Seu nome tem ao todo {} letras.'.format(len(nome) - nome.count(' ')))
+print('Seu primeiro nome tem {} letras.'.format(nome.find(' '))) # Pede para encontrar o primeiro espaço.
+
+# Ou
+
+separa = nome.split()
+print('Seu primeiro nome é {} e ele tem {} letras.'.format(separa[0], len(separa[0])))
