9bac57c07fa3675f73f880f0ba7dcd40d60b9ba0
Luissms <ldossant@gmail.com>
2026-06-27 19:03:11 -0300

    Update desafio027.py

--- cursoemvideo_python/mundo001/desafios/desafio027.py
+++ cursoemvideo_python/mundo001/desafios/desafio027.py
@@ -4,3 +4,7 @@ Ex. Ana Maria de Souza
 primeiro = Ana
 último = Souza
 """
+nome = str(input('Digite seu nome completo: ')).strip().split() # nome = n.split() na segunda linha.
+print('Muito prazer em te conhecer!')
+print('Seu primeiro nome é {}.'.format(nome[0]))
+print('Seu último nome é {}.'.format(nome[-1])) # .format(nome[len(nome)-1])
