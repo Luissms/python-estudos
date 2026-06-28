037923d462dc598bfdce1c4c207c0b49c5815f90
Luissms <ldossant@gmail.com>
2026-06-27 21:25:53 -0300

    Update desafio033.py

--- cursoemvideo_python/mundo001/desafios/desafio033.py
+++ cursoemvideo_python/mundo001/desafios/desafio033.py
@@ -1,3 +1,20 @@
 """
 Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
 """
+a = float(input('Primeiro valor: '))
+b = float(input('Segundo valor: '))
+c = float(input('Terceiro valor: '))
+# Verificando quem é menor
+menor = a
+if b<a and b<c:
+    menor = b
+if c<a and c<b:
+    menor = c
+# Verificando quem é o maior
+maior = a
+if b>a and b>c:
+    maior = b
+if c>a and c>b:
+    maior = c
+print('O menor valor digitado foi {}.'.format(menor))
+print('O maior valor digitado foi {}.'.format(maior))
