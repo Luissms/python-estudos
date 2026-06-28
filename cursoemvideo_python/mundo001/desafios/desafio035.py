47391fda2457b5d073ba0f812e6a3c4fdc378e7b
Luissms <ldossant@gmail.com>
2026-06-27 21:37:32 -0300

    Update desafio035.py

--- cursoemvideo_python/mundo001/desafios/desafio035.py
+++ cursoemvideo_python/mundo001/desafios/desafio035.py
@@ -1,3 +1,13 @@
 """
 Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.
 """
+print('-=-' * 20)
+print('Analisador de Triângulos')
+print('-=-' * 20)
+r1 = float(input('Digite a primeira reta: '))
+r2 = float(input('Digite a segunda reta: '))
+r3 = float(input('Digite a terceira reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo.')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo.')
