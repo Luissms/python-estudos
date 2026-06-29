f9a26270584d1b08eaf322fa53411b68819f780f
Luissms <ldossant@gmail.com>
2026-06-29 11:59:04 -0300

    Update desafio042.py

--- cursoemvideo_python/mundo002/desafios/desafio042.py
+++ cursoemvideo_python/mundo002/desafios/desafio042.py
@@ -1,3 +1,19 @@
 """
 Refaça o desafio 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: Equilátero: todos os lados iguais, Isósceles: dois lados iguais, Escaleno: todos os lados diferentes.
 """
+print('-=-' * 20)
+print('Analisador de Triângulos')
+print('-=-' * 20)
+r1 = float(input('Digite a primeira reta: '))
+r2 = float(input('Digite a segunda reta: '))
+r3 = float(input('Digite a terceira reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo ', end='')
+    if r1 == r2 == r3:
+        print('Equilátero.')
+    elif r1 != r2 != r3 != r1:
+        print('Escaleno.')
+    else:
+        print('Isósceles.')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR triângulo.')
