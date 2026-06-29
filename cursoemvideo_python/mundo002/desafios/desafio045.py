91c1911a0c073f8e9f789ff6867683c34ae5589c
Luissms <ldossant@gmail.com>
2026-06-29 16:50:49 -0300

    Update desafio045.py

--- cursoemvideo_python/mundo002/desafios/desafio045.py
+++ cursoemvideo_python/mundo002/desafios/desafio045.py
@@ -1,3 +1,49 @@
 """
-Crie um programa que faça o computador jogar JOKENPÔ com você.
+Crie um programa que faça o computador jogar JOKENPÔ com você. Pedra, papel ou tesoura.
 """
+from random import randint
+from time import sleep
+itens = ('Pedra', 'Papel', 'Tesoura')
+computador = randint(0, 2)
+print('''Suas opções:
+[ 0 ] Pedra
+[ 1 ] Papel
+[ 2 ] Tesoura''')
+jogador = int(input('Qual é a sua jogada? '))
+print('JO')
+sleep(1)
+print('KEN')
+sleep(1)
+print('PÔ!!!')
+sleep(1)
+print('-=' * 11)
+print('O computador jogou {}.'.format(itens[computador]))
+print('O jogador jogou {}.'.format(itens[jogador]))
+print('-=' * 11)
+if computador == 0: # Computador jogou PEDRA
+    if jogador == 0:
+        print('Empate')
+    elif jogador == 1:
+        print('Jogador vence')
+    elif jogador == 2:
+        print('Computador vence')
+    else:
+        print('Jogada inválida')
+elif computador == 1: # Computador jogou PAPEL
+    if jogador == 0:
+        print('Computador vence')
+    elif jogador == 1:
+        print('Empate')
+    elif jogador == 2:
+        print('Jogador vence')
+    else:
+        print('Jogada inválida')
+elif computador == 2: # Computador jogou TESOURA
+    if jogador == 0:
+        print('Jogador vence')
+    elif jogador == 1:
+        print('Computador vence')
+    elif jogador == 2:
+        print('Empate')
+    else:
+        print('Jogada inválida')
