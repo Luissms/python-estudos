efb99600b4964818bc7283d1dd79cb55795cad73
Luissms <ldossant@gmail.com>
2026-07-02 07:48:56 -0300

    Update ex016_tuplas.py

--- cursoemvideo_python/mundo003/aulas/ex016_tuplas.py
+++ cursoemvideo_python/mundo003/aulas/ex016_tuplas.py
@@ -16,3 +16,14 @@ for pos, comida in enumerate(lanche):
 
 print(sorted(lanche)) # coloca em ordem
 
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = b + a
+print(len(c))
+print(c.count(5)) # 2 - Quantos tem?
+print(c.index(8)) # Em que posição está?
+print(c.index(5, 1)) # Em que posição está começando a contar da posição 1.
+
+pessoa = ('Gustavo', 39, 'M', 99.88)
+del(pessoa) # Ele apaga a tupla inteira. Mas eu não posso apager somente um elemento. 
+print(pessoa) # Em outras linguagens não pode ter dados de diferentes tipos, no Python sim.
