2f1c6182214431d2dcaccf487cc3f806c4a07498
Luissms <ldossant@gmail.com>
2026-07-04 16:40:58 -0300

    Update rich002.py

--- cursoemvideo_python/rich/rich002.py
+++ cursoemvideo_python/rich/rich002.py
@@ -4,3 +4,10 @@ from rich.panel import Panel
 caixa = Panel('[white]Esse aqui é um painel de exemplo[/]:+1:', title='Mensagem', style='red', width=40, subtitle='Feito com Rich')
 
 print(caixa)
+
+
+"""
+title >> título do painel
+style >> coloca tudo em vermelho
+width >> largura do painel 
+"""
