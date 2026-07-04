5c99caf912275f8dffb96cad91d8307f4ebe0ac1
Luissms <ldossant@gmail.com>
2026-07-04 16:36:35 -0300

    Update rich001.py

--- cursoemvideo_python/rich/rich001.py
+++ cursoemvideo_python/rich/rich001.py
@@ -1,4 +1,4 @@
 from rich import print
-print("Olá, [red]Mundo![/]") # python -m rich.emoji
+print("Olá, [blue]Mundo![/]:earth_americas:") # python3 -m rich.emoji no terminal.
 print("Olá, [bold blue on white]Pequeno Gafanhoto[/] :vulcan_salute:")
 print(":+1: :martial_arts_uniform: :-1:")
